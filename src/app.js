ef1cdead81c0a25c2cb41c0767e059b114f06785
zynx0011 <kunalnachan10@gmail.com>
2024-03-09 19:52:33 +0530

    a

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: "https://frontend-blog-app-psi.vercel.app",
-    credentials: true,
+    credentials: false,
   })
 );
 // const __dirname = path.resolve();
